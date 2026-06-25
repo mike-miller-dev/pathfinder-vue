13ef8e51693c4a39841adb882cfdf98639febc52
Michael Miller <michael.miller.developer@gmail.com>
2026-06-24 20:55:27 -0400

    +4 inspire courage

--- src/components/Pathfinder.vue
+++ src/components/Pathfinder.vue
@@ -116,7 +116,7 @@ export default defineComponent({
           ] as Array<CharacterAttack>,
           partyBuffs : [
             { name: 'flagbearer',  bonuses: [ { attackMod: 1, damageMod: 1, fearSave: 1, charmSave: 1, type: 'morale' } ] },
-            { name: 'inspire courage',  bonuses: [ { attackMod: 3, damageMod: 3, fearSave: 3, type: 'competence' } ] },
+            { name: 'inspire courage',  bonuses: [ { attackMod: 4, damageMod: 4, fearSave: 4, type: 'competence' } ] },
             { name: 'good hope',  bonuses: [ { attackMod: 2, damageMod: 2, fearSave: 2, charmSave: 2, type: 'morale' } ] },
             { name: 'haste', bonuses: [ { attackMod: 1, extraAttacks: 1 } ] },
           ] as Array<Buff>,
