4c759f85c6c987b320f2cd0764437a4c86b3d5dd
Michael Miller <michael.miller.developer@gmail.com>
2026-07-15 23:44:46 -0400

    bab 13

--- src/components/Pathfinder.vue
+++ src/components/Pathfinder.vue
@@ -78,7 +78,7 @@ export default defineComponent({
       characters : [
         {
           name: 'Deebo',
-          baseAttack: 12,
+          baseAttack: 13,
           str: 27,
           attacks:  [
             { name: 'Furious Adamantine Chainsaw +2', stat: 'str', type: 'melee2h', damageDice: '3d6', crit: 15,
